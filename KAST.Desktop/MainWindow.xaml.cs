f0df832284782c4f12fb8282227da6ab7d5650f2
Keelah Atom <al.loncham@gmail.com>
2022-05-06 16:48:39 +0200

    Added Basic Code Structure

---
+++ KAST.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using KAST.Desktop.ViewModels;
+
+using System.Windows;
+
+namespace KAST.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowsVM();
+        }
+    }
+}
