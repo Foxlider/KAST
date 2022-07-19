90005a0030984053bb3a3826c29e67afb6ec3f81
Keelah <al.loncham@gmail.com>
2022-07-19 17:54:37 +0200

    Quick code fix

--- KAST.Desktop.Core/Services/SampleDataService.cs
+++ KAST.Desktop.Core/Services/SampleDataService.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using KAST.Core.Models;
+﻿using KAST.Core.Models;
 using KAST.Desktop.Core.Contracts.Services;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace KAST.Desktop.Core.Services;
 
