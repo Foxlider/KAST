5c650cda155985646b208521a8260b1cdb5635c9
Keelah Atom <al.loncham@gmail.com>
2022-09-27 14:46:53 +0200

    Add classes

---
+++ KAST.Domain/Entities/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace KAST.Domain.Entities
+{
+    public class Author : AuditableEntity
+    {
+        public ulong AuthorID { get; internal set; }
+        public string? Name { get; internal set; }
+        public string? URL { get; internal set; }
+    }
+}
---
+++ KAST.Domain/Entities/Mod.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace KAST.Domain.Entities
+{
+    public class Mod : AuditableEntity
+    {
+        public ulong Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public virtual Author? Author { get; set; }
+        public string? Path { get; set; }
+        public DateTime? SteamLastUpdated{ get; set; }
+        public DateTime? LocalLastUpdated { get; set; }
+
+    }
+}
