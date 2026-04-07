22bdb53f9f443de0dbd230e70927fa975b32924f
Keelah Atom <al.loncham@gmail.com>
2026-04-07 14:41:32 +0200

    KAST.Web blazor setup

--- src/KAST.Web/Program.cs
+++ src/KAST.Web/Program.cs
@@ -1,6 +1,54 @@
+using KAST.Infrastructure;
+using KAST.Infrastructure.Data;
+using KAST.Web.Hubs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.FluentUI.AspNetCore.Components;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database
+var connectionString = builder.Configuration.GetConnectionString("Default") ?? "Data Source=kast.db";
+builder.Services.AddKastInfrastructure(connectionString);
+
+// Blazor Server
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+
+// API
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// SignalR
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Auto-migrate database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<KastDbContext>();
+    await db.Database.MigrateAsync();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+// Map API controllers under /api
+app.MapControllers();
+
+// Map SignalR hubs
+app.MapHub<MonitoringHub>("/hubs/monitoring");
+app.MapHub<DownloadHub>("/hubs/downloads");
+
+// Map Blazor
+app.MapRazorComponents<KAST.Web.Components.App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
